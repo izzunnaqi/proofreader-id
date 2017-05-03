6093d3a2487196ae97a0a3c756fff38eeed90620
izzunnaqi <izzunnaqi@gmail.com>
2017-05-04 06:59:06 +0700

    New format for xml input

--- src/checker.py
+++ src/checker.py
@@ -13,29 +13,29 @@ def check_dots(pages):
 			elif len(re.findall('\s[A-Z]{1,2}\.[a-zA-Z]{1,3}\.[a-zA-Z]{0,3}\.?', pages[i])) > 0:
 				row = row;
 			else:
-				row = row + str(i) + ", "
+				row = row + str(i+1) + ", "
 				print "kena 1: " 
 				print re.findall('[a-zA-Z]+\.[a-zA-Z]+', pages[i])
 
 		if len(re.findall('[1-9]\.[1-9]\.[a-zA-Z]+', pages[i])) > 0:
-			row = row + str(i) + ", "
+			row = row + str(i+1) + ", "
 			print "kena 2: " 
 			print re.findall('[1-9]\.[1-9]\.[a-zA-Z]+', pages[i])
 
 		if len(re.findall('[1-9]\.[1-9]\.[1-9]\.[a-zA-Z]+', pages[i])) > 0:
-			row = row + str(i) + ", "
+			row = row + str(i+1) + ", "
 			print "kena 3: " 
 			print re.findall('[1-9]\.[1-9]\.[1-9]\.[a-zA-Z]+', pages[i])
 
 		if len(re.findall('(.*)[1-9]+\.\s[0-9][a-zA-Z]*', pages[i])) > 0:
-			row = row + str(i) + ", "
+			row = row + str(i+1) + ", "
 			print "kena 4: " 
 			print re.findall('(.*)[1-9]+\.\s[0-9][a-zA-Z]*', pages[i])
 	
 	if len(row) < 1:
 		row = ""
 	else:
-		row = "pada baris " + row[0:-2]
+		row = "pada kalimat " + row[0:-2]
 
 	return row
 
@@ -62,17 +62,38 @@ def main():
 	in_file = sys.argv[1]
 
 	tree = et.parse(in_file)
-	page = tree.getroot()
-	pages = page.findall('page')
+	root = tree.getroot()
+	pages = root.findall('page')
 
-	for i in range(0, len(pages)):
-		dots = check_dots(pages[i].text.splitlines())
+	
+	for e in pages:
+		sentences = e.findall('sentence')
+		temp = []
+		
+
+		for s in sentences:
+			temp.append(s.text)
+
+		
+		dots = check_dots(temp)
 		if len(dots) > 0:
-			print "kesalahan titik pada halaman "+ str(i+1) +" "+dots
+			print "kesalahan titik pada halaman "+ e.attrib['id'] +" "+dots
 
-		comma = check_comma(pages[i].text.splitlines())
+		comma = check_comma(temp)
 		if len(comma) > 0:
-			print "kesalahan koma pada halaman "+ str(i+1) +" "+comma
+			print "kesalahan koma pada halaman "+ e.attrib['id'] +" "+comma
+
+	
+
+
+	# for i in range(0, len(pages)):
+	# 	dots = check_dots(pages[i].text.splitlines())
+	# 	if len(dots) > 0:
+	# 		print "kesalahan titik pada halaman "+ str(i+1) +" "+dots
+
+	# 	comma = check_comma(pages[i].text.splitlines())
+	# 	if len(comma) > 0:
+	# 		print "kesalahan koma pada halaman "+ str(i+1) +" "+comma
 
 if __name__ == '__main__':
 	main()
--- src/xmlparse.py
+++ src/xmlparse.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -W ignore::DeprecationWarning
 # -*- coding: utf-8 -*-qq
 
 import sys
 import re
 import xml.etree.ElementTree as et
+from nltk import tokenize
 
 def config():
 	reload(sys)
@@ -23,29 +24,48 @@ def extract_xml(argv):
 	textbox = page.findall("textbox")
 
 	temp = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<document>\n"
+	
+	# read page
 	for p in allpage:
-		temp = temp + '<page id=\"'+ p.attrib['id']+'\">'
+		temp = temp + '<page id=\"'+ p.attrib['id']+'\">\n'
+		page_str = ""
 
+		# read line
 		for el in p.findall("textbox"):
+			line = ""
+			
 			for t in el.findall("textline/text"):
 				if 'bbox' in t.attrib:
 					if t.text is None:
-						temp = temp + " "
+						# temp = temp + " "
+						line = line + " "
 					else:
 						if t.text == '&':
-							temp = temp + '&amp;'
+							# temp = temp + '&amp;'
+							line = line + '&amp;'
 						elif t.text == '>':
-							temp = temp + "&gt;"
+							# temp = temp + "&gt;"
+							line = line + "&gt;"
 						elif t.text == '<':
-							temp = temp + "&lt;"
+							# temp = temp + "&lt;"
+							line = line + "&lt;"
 						else:
-							temp = temp + t.text
+							# temp = temp + t.text
+							line = line + t.text
 
-					# temp = temp.rstrip("\n" )
-
-			temp = temp + '\n'
-
-		temp = temp + '</page>\n'
+			# temp = temp + '\n'
+			page_str = page_str + line
+			# sentences = tokenize.sent_tokenize(line)
+			# for i in range(0, len(sentences)):
+			# 	print "<sentence id=\"" + str(i) +"\">" +sentences[i]+"</sentence>"
+		
+	
+		sentences = tokenize.sent_tokenize(page_str)
+		page_str = ""
+		for i in range(0, len(sentences)):
+			page_str = page_str + "<sentence id=\"" + str(i+1) +"\">" +sentences[i]+"</sentence>\n"
+			
+		temp = temp + page_str + '</page>\n'
 
 		num = int(p.attrib['id'])
 		pages[num] = temp.splitlines()
