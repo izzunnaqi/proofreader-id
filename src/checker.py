0c5233fc71f9c2f5744a7f08a87752e4db77b0c4
izzunnaqi <izzunnaqi@gmail.com>
2017-05-04 09:00:04 +0700

    Added colon and semicolon check

--- src/checker.py
+++ src/checker.py
@@ -2,9 +2,11 @@
 import sys
 import re
 import xml.etree.ElementTree as et
+import nltk
 
 def check_dots(pages):
 	row = ""
+	mistakes = ""
 
 	for i in range(0, len(pages)):
 		if len(re.findall('[a-zA-Z]+\.[a-zA-Z]+', pages[i])) > 0:
@@ -14,49 +16,78 @@ def check_dots(pages):
 				row = row;
 			else:
 				row = row + str(i+1) + ", "
-				print "kena 1: " 
-				print re.findall('[a-zA-Z]+\.[a-zA-Z]+', pages[i])
+				mistakes = mistakes + ('\n').join(re.findall('[a-zA-Z]+\.[a-zA-Z]+', pages[i]))
 
 		if len(re.findall('[1-9]\.[1-9]\.[a-zA-Z]+', pages[i])) > 0:
 			row = row + str(i+1) + ", "
-			print "kena 2: " 
-			print re.findall('[1-9]\.[1-9]\.[a-zA-Z]+', pages[i])
+			mistakes = mistakes + ('\n').join(re.findall('[1-9]\.[1-9]\.[a-zA-Z]+', pages[i]))
 
 		if len(re.findall('[1-9]\.[1-9]\.[1-9]\.[a-zA-Z]+', pages[i])) > 0:
 			row = row + str(i+1) + ", "
-			print "kena 3: " 
 			print re.findall('[1-9]\.[1-9]\.[1-9]\.[a-zA-Z]+', pages[i])
 
 		if len(re.findall('(.*)[1-9]+\.\s[0-9][a-zA-Z]*', pages[i])) > 0:
 			row = row + str(i+1) + ", "
-			print "kena 4: " 
 			print re.findall('(.*)[1-9]+\.\s[0-9][a-zA-Z]*', pages[i])
 	
 	if len(row) < 1:
 		row = ""
 	else:
-		row = "pada kalimat " + row[0:-2]
+		row = "kalimat " + row[0:-2]
 
-	return row
+	return mistakes
 
 def check_comma(pages):
 	row = ""
 
 	for i in range(0, len(pages)):
 		if len(re.findall('[a-zA-Z]+\,[a-zA-Z]+', pages[i])) > 0:
-			row = row + str(i) + ", "
+			row = row + str(i+1) + ", "
 			print re.findall('[a-zA-Z]+\,[a-zA-Z]+', pages[i])
 
 		if len(re.findall('dan\,', pages[i])) > 0:
-			row = row + str(i) + ", "
+			row = row + str(i+1) + ", "
 			print re.findall('dan\,', pages[i])
 
 	if len(row) < 1:
 		row = ""
 	else:
-		row = "pada baris " + row[0:-2]
+		row = "kalimat " + row[0:-2]
 
 	return row	
+
+def check_semicolon(pages):
+	row = ""
+
+	for i in range(0, len(pages)):
+		if len(re.findall('[a-zA-Z]+\;[a-zA-Z]+', pages[i])) > 0:
+			row = row + str(i+1) + ", "
+			print re.findall('[a-zA-Z]+\;[a-zA-Z]+', pages[i])
+
+	if len(row) < 1:
+		row = ""
+	else:
+		row = "kalimat " + row[0:-2]
+
+	return row	
+
+def check_colon(pages):
+	row = ""
+
+	for i in range(0, len(pages)):
+		if len(re.findall('[a-zA-Z]+\:[a-zA-Z]+', pages[i])) > 0:
+			row = row + str(i+1) + ", "
+			print re.findall('[a-zA-Z]+\:[a-zA-Z]+', pages[i])
+
+	if len(row) < 1:
+		row = ""
+	else:
+		row = "kalimat " + row[0:-2]
+
+	return row	
+
+def check_bracket(pages):
+	pass
 
 def main():
 	in_file = sys.argv[1]
@@ -77,23 +108,20 @@ def main():
 		
 		dots = check_dots(temp)
 		if len(dots) > 0:
-			print "kesalahan titik pada halaman "+ e.attrib['id'] +" "+dots
+			print "kesalahan titik pada halaman "+ e.attrib['id'] +"\n"+dots
 
 		comma = check_comma(temp)
 		if len(comma) > 0:
 			print "kesalahan koma pada halaman "+ e.attrib['id'] +" "+comma
 
+		semicolon = check_semicolon(temp)
+		if len(semicolon) > 0:
+			print "kesalahan titik koma pada halaman "+ e.attrib['id'] +" "+semicolon
+
+		colon = check_colon(temp)
+		if len(colon) > 0:
+			print "kesalahan titik dua pada halaman "+ e.attrib['id'] +" "+colon
 	
-
-
-	# for i in range(0, len(pages)):
-	# 	dots = check_dots(pages[i].text.splitlines())
-	# 	if len(dots) > 0:
-	# 		print "kesalahan titik pada halaman "+ str(i+1) +" "+dots
-
-	# 	comma = check_comma(pages[i].text.splitlines())
-	# 	if len(comma) > 0:
-	# 		print "kesalahan koma pada halaman "+ str(i+1) +" "+comma
 
 if __name__ == '__main__':
 	main()
