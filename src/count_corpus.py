1e67096e3f682dcdf08999ece674f11fe7389dce
izzunnaqi <izzunnaqi@gmail.com>
2017-10-31 12:34:46 +0700

    Added error corpus

---
+++ src/count_corpus.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python -W ignore::DeprecationWarning
+# -*- coding: utf-8 -*-qq
+
+for i in xrange(20, 101):
+	print "<PAIR id=\"" + str(i) + "\">"
+	print "<MISTAKE></MISTAKE>"
+	print "<CORRECTION></CORRECTION>"
+	print "<ERRTYPE></ERRTYPE>"
+	print "</PAIR>\n"
+
